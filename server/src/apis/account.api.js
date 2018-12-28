29b7c6bb1710e1a362d6f7a5893ef1af975c6e16
bachlx <bachlx@bitmark.com>
2018-12-28 13:41:45 +0700

    update apis

--- server/src/apis/account.api.js
+++ server/src/apis/account.api.js
@@ -10,9 +10,9 @@ module.exports = {
   getIdentity: (req, res) => {
     let accountNumber = req.params.accountNumber;
     if (accountNumber && config.map_identities[accountNumber]) {
-      return res.send({
-        identities: { accountNumber: config.map_identities[accountNumber] }
-      });
+      let identities = {};
+      identities[accountNumber] = config.map_identities[accountNumber];
+      return res.send({ identities });
     }
     res.send({ identities: {} });
   },
